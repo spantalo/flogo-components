d306e1575b71c7766ec1653c7b3e10e57efd8c41
spantalo <spantalo@tibco.com>
2022-09-09 14:17:44 +0200

    reengineered

--- activity/parquetfilewriter/activity.go
+++ activity/parquetfilewriter/activity.go
@@ -9,60 +9,54 @@ import (
 
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
 	"github.com/TIBCOSoftware/flogo-lib/core/data"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
 
 	"github.com/xitongsys/parquet-go-source/local"
 	"github.com/xitongsys/parquet-go/writer"
 )
 
-// Activity is a Parquet writer
-type Activity struct {
+var log = logger.GetLogger("flogo-parquet-filewriter")
+var versionPrinted = false
+
+// PutActivity is a stub for your Activity implementation
+type FileWriterActivity struct {
+	metadata *activity.Metadata
 }
 
-func init() {
-	_ = activity.Register(&Activity{}, New)
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	//TODO REMOVE
+	//log.SetLogLevel(logger.DebugLevel)
+	//
+	return &FileWriterActivity{metadata: metadata}
 }
 
-var activityMd = activity.ToMetadata(&Input{}, &Output{})
-
-// Metadata returns the activity's metadata
-func (a *Activity) Metadata() *activity.Metadata {
-	return activityMd
-}
-
-// New create a new  activity
-func New(ctx activity.InitContext) (activity.Activity, error) {
-	act := &Activity{}
-	return act, nil
+// Metadata implements activity.Activity.Metadata
+func (a *FileWriterActivity) Metadata() *activity.Metadata {
+	return a.metadata
 }
 
 // Eval implements activity.Activity.Eval
-func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
-
-	ctx.Logger().Infof("Processing file: %s", parquetFile)
+func (a *FileWriterActivity) Eval(ctx activity.Context) (done bool, err error) {
 
 	// Get the runtime values
-	in := &Input{}
-	err = ctx.GetInputObject(in)
-	if err != nil {
-		return false, err
-	}
-
-	jsonSchema := in.JSONSchema
-	jsonString := in.JSONString
-	parquetFile := in.ParquetFile
+	jsonSchema := ctx.GetInput("jsonschema").(string)
+	jsonString := ctx.GetInput("jsonstring").(string)
+	parquetFile := ctx.GetInput("filename").(string)
+	log.Infof("Processing file: %s", parquetFile)
 
 	if ctx.GetInput("ContentJson") != nil {
 		messageJSONObj := ctx.GetInput("ContentJson").(*data.ComplexObject)
 		buffer, err := json.Marshal(messageJSONObj.Value)
 		if err != nil {
-			ctx.Logger().Errorf("Failed to decode map input for reason [%s]", err)
+			log.Errorf("Failed to decode map input for reason [%s]", err)
 			return false, fmt.Errorf("Failed to decode map input for reason [%s]", err)
 		}
 		jsonString = string(buffer)
 	}
 
-	//ctx.Logger().Debugf("jsonSchema %s", jsonSchema)
-	ctx.Logger().Infof("jsonString: %s", jsonString)
+	//log.Debugf("jsonSchema %s", jsonSchema)
+	log.Infof("jsonString: %s", jsonString)
 
 	//--
 
@@ -70,33 +64,30 @@ func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
 
 	fw, err := local.NewLocalFileWriter(parquetFile)
 	if err != nil {
-		ctx.Logger().Errorf("Can't create file %s", err)
+		log.Errorf("Can't create file %s", err)
 		return false, err
 	}
 
 	pw, err := writer.NewJSONWriter(jsonSchema, fw, 4)
 	if err != nil {
-		ctx.Logger().Errorf("Can't create JSON writer %s", err)
+		log.Errorf("Can't create JSON writer %s", err)
 		return false, err
 	}
 
 	if err = pw.Write(jsonString); err != nil {
-		ctx.Logger().Errorf("Write file Error %s", err)
+		log.Errorf("Write file Error %s", err)
 	}
 
 	if err = pw.WriteStop(); err != nil {
-		ctx.Logger().Errorf("Close file Error %s", err)
+		log.Errorf("Close file Error %s", err)
 	}
 
 	fw.Close()
 
-	ctx.Logger().Infof("File completed %s", parquetFile)
+	log.Infof("File completed %s", parquetFile)
 
-	//--
-	output := &Output{}
-	output.Result = "OK"
+	err = ctx.SetOutput("result", "OK")
 
-	err = ctx.SetOutputObject(output)
 	if err != nil {
 		return false, err
 	}
